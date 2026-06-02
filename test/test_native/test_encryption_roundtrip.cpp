3dd71f85895813bed3cb103abb02365fc8207aec
Systemsplanet <132013087+Systemsplanet@users.noreply.github.com>
2026-06-01 22:08:06 -0400

    gitupload: test/test_native/test_encryption_roundtrip.cpp

---
+++ test/test_native/test_encryption_roundtrip.cpp
@@ -0,0 +1,39 @@
+#include <unity.h>
+#include "NxProtocol.h"
+#include "NxLoopback.h"
+#include <vector>
+#include <cstring>
+
+using namespace nowx;
+
+static void encryption_roundtrip() {
+  LoopbackBus bus;
+  uint8_t macA[6] = {9,0,0,0,0,1};
+  uint8_t macB[6] = {9,0,0,0,0,2};
+  LoopbackEndpoint epA(&bus, macA);
+  LoopbackEndpoint epB(&bus, macB);
+  NxProtocol pa(&epA), pb(&epB);
+  pa.setPeer(macB);
+  pb.setPeer(macA);
+
+  uint8_t key[32];
+  for (int i = 0; i < 32; i++) key[i] = (uint8_t)(0x11 * i + 1);
+  pa.setKey(key, 32);
+  pb.setKey(key, 32);
+
+  std::vector<uint8_t> payload(600);
+  for (size_t i = 0; i < payload.size(); i++) payload[i] = (uint8_t)(i ^ 0xA5);
+
+  TEST_ASSERT_TRUE(pa.send(payload.data(), (uint32_t)payload.size(), NX_ENCRYPT));
+
+  Message m;
+  TEST_ASSERT_TRUE(pb.receive(m));
+  TEST_ASSERT_EQUAL_UINT32((uint32_t)payload.size(), m.len());
+  TEST_ASSERT_TRUE(m.encrypted());
+  TEST_ASSERT_EQUAL_INT8_ARRAY(payload.data(), m.data(), payload.size());
+  // m released by destructor
+}
+
+void run_encryption_roundtrip_tests() {
+  RUN_TEST(encryption_roundtrip);
+}
