3da50e6e8acfef3e583618801fa2a7e394ce01dd
Systemsplanet <132013087+Systemsplanet@users.noreply.github.com>
2026-06-01 22:08:12 -0400

    gitupload: test/test_native/test_main.cpp

---
+++ test/test_native/test_main.cpp
@@ -0,0 +1,32 @@
+#include <unity.h>
+
+void setUp(void)    {}
+void tearDown(void) {}
+
+void run_crc_tests();
+void run_encrypt_tests();
+void run_mac_tests();
+void run_fragmentation_tests();
+void run_reassembly_tests();
+void run_ack_window_tests();
+void run_encryption_roundtrip_tests();
+void run_message_str_tests();
+void run_logger_tests();
+void run_gc_timeout_tests();
+void run_rxq_full_tests();
+
+int main(int, char**) {
+  UNITY_BEGIN();
+  run_crc_tests();
+  run_encrypt_tests();
+  run_mac_tests();
+  run_fragmentation_tests();
+  run_reassembly_tests();
+  run_ack_window_tests();
+  run_encryption_roundtrip_tests();
+  run_message_str_tests();
+  run_logger_tests();
+  run_gc_timeout_tests();
+  run_rxq_full_tests();
+  return UNITY_END();
+}
