43a96608b5b8bbad25a06662ed717e28489dbed4
Systemsplanet <132013087+Systemsplanet@users.noreply.github.com>
2026-06-01 22:07:38 -0400

    gitupload: examples/Sender/Sender.ino

---
+++ examples/Sender/Sender.ino
@@ -0,0 +1,22 @@
+#include <NowX.h>
+
+NowX now("tx");
+
+void setup() {
+  Serial.begin(115200);
+  delay(200);
+  if (!now.begin()) {
+    Serial.println("ESP-NOW init failed");
+    while (true) delay(1000);
+  }
+  // Replace with your receiver's MAC address.
+  now.setPeer("AA:BB:CC:DD:EE:FF");
+}
+
+void loop() {
+  static uint32_t n = 0;
+  String s = String("hello #") + n++;
+  bool ok = now.send(s);
+  Serial.printf("send -> %s\n", ok ? "ok" : "FAIL");
+  delay(1000);
+}
